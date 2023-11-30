a655fb15f4393567913de2c9617c3ddd55f1624d
mm9717 <100307044+mm9717@users.noreply.github.com>
2023-11-30 06:23:55 +0400

    Added Country class and required main function for countryclass.py

--- countryclass.py
+++ countryclass.py
@@ -43,3 +43,72 @@ class Country:
     def setEducation(self, education):
         self.__country_name = education
 
+class HappinessMeter:
+    def __init__(self):
+        """
+        Initialize HappinessMeter object with an empty list of countries.
+        """
+        self.countries = []
+
+    def addCountry(self, country):
+        """
+        Add a country to the HappinessMeter.
+
+        """
+        self.countries.append(country)
+
+    def measure_happiness(self):
+        """
+        Measuring the happiness of each country based on factors in the Country class.
+
+        Returns:
+        Tuples containing country name and each of ther happiness scores
+        """
+        happiness_scores = []
+
+        for country in self.countries:
+            happiness_score = (
+                country.getEnvironment()
+                + country.getEconomy()
+                + country.getCulture()
+                + country.getHealthcare()
+                + country.getEducation()
+            )
+
+            happiness_scores.append((country.getCountryName(), happiness_score))
+
+        return happiness_scores
+
+
+def main():
+    # Create an instance of the HappinessMeter class
+    happiness_meter = HappinessMeter()
+
+    # Prompt the user to enter the number of countries they want to assess
+    num_countries = int(input("Enter the number of countries you want to assess: "))
+
+    # Collect data of each country and add them to the HappinessMeter
+    for _ in range(num_countries):
+        country_name = input("Enter the name of the country: ")
+        environment = int(input("Enter the environmental factor (1-10): "))
+        economy = int(input("Enter the economic factor (1-10): "))
+        culture = int(input("Enter the cultural factor (1-10): "))
+        healthcare = int(input("Enter the healthcare factor (1-10): "))
+        education = int(input("Enter the educational factor (1-10): "))
+
+        # Create an instance of the Country class with user provided data for parameters
+        country = Country(country_name, environment, economy, culture, healthcare, education)
+
+        # Add country to the HappinessMeter
+        happiness_meter.addCountry(country)
+
+    # Measure the happiness and print the results
+    happiness_scores = happiness_meter.measure_happiness()
+    for country, score in happiness_scores:
+        print(f"{country}: {score}")
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()
+
