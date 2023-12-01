547e0292f4fb7eb1a498923362cf54d08b6a8b49
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2023-12-01 17:15:37 +0400

    Updated the measure happiness function to calculate average countryclass.py

--- countryclass.py
+++ countryclass.py
@@ -1,4 +1,7 @@
 class Country:
+    '''
+    this is a class that represents a country
+    '''
     def __init__(self, country_name, enviroment, economy, culture, healthcare, education):
         self.__country_name = country_name
         self.__enviroment = enviroment
@@ -8,39 +11,75 @@ class Country:
         self.__education = education
     
     def getCountryName(self):
+        '''
+        returns the name of the country
+        '''
         return self.__country_name
     
     def setCountryName(self, country_name):
+        '''
+        sets up the variable of the country name
+        '''
         self.__country_name = country_name
 
     def getEnvironment(self):
+        '''
+        returns the environment rating of the country
+        '''
         return self.__enviroment
     
     def setEnvironment(self, enviroment):
+        '''
+        sets up the variable of the environment rating
+        '''
         self.__country_name = enviroment
 
     def getEconomy(self):
+        '''
+        returns the economy rating of the country
+        '''
         return self.__economy
     
     def setEconomy(self, economy):
+        '''
+        sets up the variable of the economy rating
+        '''
         self.__country_name = economy
 
     def getCulture(self):
+        '''
+        returns the culture rating of the country
+        '''
         return self.__culture
     
     def setCulture(self, culture):
+        '''
+        sets up the variable of the culture rating
+        '''
         self.__country_name = culture
 
     def getHealthcare(self):
+        '''
+        returns the healthcare rating of the country
+        '''
         return self.__healthcare
     
     def setHealthcare(self, healthcare):
+        '''
+        sets up the variable of the healthcare rating
+        '''
         self.__country_name = healthcare
 
     def getEducation(self):
+        '''
+        returns the education rating of the country
+        '''
         return self.__education
     
     def setEducation(self, education):
+        '''
+        sets up the variable of the education rating
+        '''
         self.__country_name = education
 
 class HappinessMeter:
@@ -53,30 +92,19 @@ class HappinessMeter:
     def addCountry(self, country):
         """
         Add a country to the HappinessMeter.
-
         """
         self.countries.append(country)
 
     def measure_happiness(self):
         """
-        Measuring the happiness of each country based on factors in the Country class.
+        Measuring the happiness of each country based on the average factors in the Country class.
 
-        Returns:
-        Tuples containing country name and each of ther happiness scores
+        returns a list of tuples with the country name and happiness score.
         """
         happiness_scores = []
-
         for country in self.countries:
-            happiness_score = (
-                country.getEnvironment()
-                + country.getEconomy()
-                + country.getCulture()
-                + country.getHealthcare()
-                + country.getEducation()
-            )
-
-            happiness_scores.append((country.getCountryName(), happiness_score))
-
+            happiness_score = (country.getCountryName(), (country.getEnvironment() + country.getEconomy() + country.getCulture() + country.getHealthcare() + country.getEducation()) / 5)
+            happiness_scores.append(happiness_score)
         return happiness_scores
 
 
@@ -89,12 +117,12 @@ def main():
 
     # Collect data for each country and add them to the HappinessMeter
     for _ in range(num_countries):
-        country_name = input("Enter the name of the country: ")
-        environment = int(input("Enter the environmental factor (1-10): "))
-        economy = int(input("Enter the economic factor (1-10): "))
-        culture = int(input("Enter the cultural factor (1-10): "))
-        healthcare = int(input("Enter the healthcare factor (1-10): "))
-        education = int(input("Enter the educational factor (1-10): "))
+        country_name = input("\nEnter the name of the country: ")
+        environment = int(input("Enter the environmental factor (1-100): "))
+        economy = int(input("Enter the economic factor (1-100): "))
+        culture = int(input("Enter the cultural factor (1-100): "))
+        healthcare = int(input("Enter the healthcare factor (1-100): "))
+        education = int(input("Enter the educational factor (1-100): "))
 
         # Create an instance of the Country class with user provided data for parameters
         country = Country(country_name, environment, economy, culture, healthcare, education)
@@ -102,8 +130,11 @@ def main():
         # Add country to the HappinessMeter
         happiness_meter.addCountry(country)
 
-    # Measure the happiness and print the results
+    # Measure the happiness by averaging the factors for each country
     happiness_scores = happiness_meter.measure_happiness()
+
+    # Print the happiness scores for each country
+    print("\nHappiness scores:")
     for country, score in happiness_scores:
         print(f"{country}: {score}")
 
